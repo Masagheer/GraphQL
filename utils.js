6994885580d511e72d7d66610c8bdf0efe04047d
Masagheer <Maramsagheer2@gmail.com>
2025-02-03 21:02:04 +0300

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,5 @@
+console.log('utils.js loaded');
+
 export function capitalizeFirstLetter(string) {
     if (!string) return 'N/A';
     return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
